ab1857e0e06ed10831dbe2b5f329179e04d9232f
RuberDavid <fenixoscar@gmail.com>
2019-10-30 09:28:09 -0600

    sigue el desarrollo de función right_eq_replaceLastMv

--- AsistenteDeGato.c
+++ AsistenteDeGato.c
@@ -1,6 +1,5 @@
-/*****************************************************************************
- *
- *****************************************************************************/
+#define BUILDING 0 //TODO: quitar este y los #if
+
 /*****************************************************************************
  *	Diseño del tablero:
  *
@@ -15,18 +14,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "bintree.h"
 
+#define NUM_OF_MOVES 9
+
 
 //arreglo global 
-char tablero[]="ABCDEFGHI";
+char Movimientos[]="ABCDEFGHI";
 
-typedef struct _info
+typedef struct _Info
 {
 	char* moves;
 	double probOfWin;
-} info;
+} Info;
+
+/*****************************************************************************
+ *
+ *	Inicializa un Info
+ *	ADVERTENCIA: no aloja memoria para el miembro moves
+ *
+ *****************************************************************************/
+void info_init( Info* myinfo, char* mystring, double myfloat );
+
 
 /*****************************************************************************
  *
@@ -39,7 +50,7 @@ typedef struct _info
 
 /*****************************************************************************
  *	
- *	Función para borrar datos de nodos del tipo info
+ *	Función para borrar datos de nodos del tipo Info
  *
  *****************************************************************************/
 void destroy(void* data);
@@ -75,14 +86,22 @@ int main()
 	BinTree arbolDJuego;
 
 	bintree_init( &arbolDJuego, destroy );
-	
+#if BUILDING
 	fillMoves( &arbolDJuego );
 
 
-
+#endif
 	bintree_destroy( &arbolDJuego );
 }
 
+void info_init( Info* myinfo, char* mystring, double myfloat )
+{
+
+	myinfo->moves= mystring;
+	myinfo->probOfWin= myfloat;
+
+	return;
+}
 
 void destroy(void* data)
 {	
@@ -102,16 +121,26 @@ void fillMoves( BinTree *tree)
 	void left_eq_concatNextMv(   BinTree *sometree, BinTreeNode *someNode);
 
 	BinTreeNode *node;
-	char * fstMv; //first Move
+	Info inforoot;
+	char* mystring;
 
-	if( (fstMv = (char*)malloc( sizeof(char) + 1 ) ) == NULL )
+
+	if( (inforoot = (Info*)malloc( sizeof(Info) ) ) == NULL )
+	{
+		fprintf(stderr,"error al alojar memoria");
+		return;
+	}
+	if( (mystring = (Info*)malloc( sizeof(char)*2 ) ) == NULL )
 	{
 		fprintf(stderr,"error al alojar memoria");
 		return;
 	}
 
+
+	info_init( inforoot, mystring , 0.0 ); 
+
 	//insertar el primer movimiento en  la raiz
-	bintree_ins_left( tree, NULL, fstMv );
+	bintree_ins_left( tree, NULL, inforoot ); 
 
 	node= bintree_root(tree);
 
@@ -123,22 +152,57 @@ void fillMoves( BinTree *tree)
 }
 /*****************************************************************************
  *
- * 	Inserta en el nodo derecho un nodo otra posible jugada actual
+ * 	Inserta en el nodo derecho un nodo con otra posible jugada actual
+ * 	(remplaza la anterior)
  * 	y se llama a si misma y a left_eq_concatNextMv en sus hijos
  *
  *****************************************************************************/
 
 void right_eq_replaceLastMv( BinTree *sometree, BinTreeNode *someNode)
 {
-	char* othermove;
+	bool not_moved( char move, char* done , size_t len );//TODO
+
+	Info *infonode;//TODO  inicializar y pasar
+	char* other=NULL;
+	char last[NUM_OF_MOVES];
+	char next='0';
 	size_t len;
-	len=strlen( bintree_moves( someNode ) );
-	if( (othermove = (char*)malloc( len*sizeof(char) ) ) == NULL )//SPECIFY width
+
+	if( (infonode = (Info*)malloc( sizeof(Info) ) ) == NULL )
+	{
+		fprintf(stderr,"error al alojar memoria");
+		return;
+	}
+	
+	//copia la cadena del nodo pasado para facilitar el manejo
+	strcpy( last, bintree_moves( someNode ) );
+	len=strlen( last );
+	if( (other = (char*)malloc( sizeof(char)*(len) ) == NULL ))
 	{
 		fprintf(stderr,"error al alojar memoria");
 		return;
 	}
 
+	//encuéntra el siguiente movimiento posible
+	for(size_t i=0; i<NUM_OF_MOVES ; i++)
+	{
+		//Busca en el arreglo global de jugadas
+		//si ya se ha hecho la i-esima jugada
+		//si no, se seleccióna como la que hay que remplazar
+		//y se sale del for
+		if( not_moved( Movimientos[i], last, len) )
+		{
+			next=Movimientos[i];
+			break;
+		}
+	}
+
+	
+	if( next != '0' )
+	{
+		last[len]=next;
+		strcpy(other, last );
+		bintree_ins_right( 
 
 	bintree_ins_right(
 	
@@ -146,8 +210,8 @@ void right_eq_replaceLastMv( BinTree *sometree, BinTreeNode *someNode)
 /*****************************************************************************
  *
  * 	Inserta en el nodo izquierdo un nodo con la siguiente posible jugada
- * 	(del otro jugador y se llama a si misma y a left_eq_concatNextMv en
- * 	sus hijos
+ * 	(del otro jugador)
+ * 	y se llama a si misma y a left_eq_concatNextMv en sus hijos
  *
  *****************************************************************************/
 
